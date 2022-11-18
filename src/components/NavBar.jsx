59947247bc4734a10b340ffbdb66a91258037b54
Ewerton da Costa Vaz <ewerton.vaz@gmail.com>
2022-11-18 16:19:16 -0300

    criada a estrutura básica dos arquivos e pastas

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,7 @@
+function NavBar() {
+    return ( <div>
+        <h1>Nav Bar</h1>
+    </div> );
+}
+
+export default NavBar;
---
+++ src/pages/CadastroPage.jsx
@@ -0,0 +1,7 @@
+function CadastroPage() {
+    return ( <div>
+        <h1>Cadastro Page</h1>
+    </div> );
+}
+
+export default CadastroPage;
