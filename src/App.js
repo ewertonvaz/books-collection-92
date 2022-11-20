54c4dbbe27db9b79fa87adadbaf64345d8f97a0a
robertomalheiros <robertomalheirosb@gmail.com>
2022-11-20 14:11:22 -0300

    Roberto - Criei a Página Ultima Página, coloquei um link de teste na DetalhesPag, criei um CSS individual para UltimaPag, e instalei a biblioteca react-pdf para leitura do PDF

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
-import './App.css';
-import { Toaster } from 'react-hot-toast';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import CadastroPage from './pages/CadastroPage';
-import EditarPage from './pages/EditarPage';
-import GoogleBooksPage from './pages/GoogleBooksPage';
-import DetalhesPage from './pages/DetalhesPage';
-import NavBar from './components/NavBar';
+import "./App.css";
+import { Toaster } from "react-hot-toast";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CadastroPage from "./pages/CadastroPage";
+import EditarPage from "./pages/EditarPage";
+import GoogleBooksPage from "./pages/GoogleBooksPage";
+import DetalhesPage from "./pages/DetalhesPage";
+import NavBar from "./components/NavBar";
+import UltimaPag from "./pages/UltimaPag";
 
 function App() {
   return (
     <div className="App">
       <Toaster />
       <NavBar />
-
-      <div className='container'>
-
+      <div className="container">
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/livro/cadastro" element={<CadastroPage />} />
           <Route path="/livro/:livroID" element={<DetalhesPage />} />
           <Route path="/livro/:livroID/editar" element={<EditarPage />} />
           <Route path="/pesquisar" element={<GoogleBooksPage />} />
+          <Route path="/livro/ultimapag/xx" element={<UltimaPag />} />
         </Routes>
       </div>
-
     </div>
   );
 }
