224eec43bb4d5b294696900e88dc3b1d5029dc04
Priscila Franco <francopri@hotmail.com>
2022-11-20 15:10:30 -0300

    inclusao btn adicionar na homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,11 +8,9 @@ function HomePage() {
 
     return (
         <div className="container">
-
             <div className="px-4">
-
-                <div className="row justify-content-center">
-                    <div className="col-8">
+                <div className="row justify-content-between">
+                    <div className="col-5">
                         <form className="form-inline">
                             <div className="input-group">
                                 <input className="form-control" type="text" placeholder="" name="pesquisar" />
@@ -22,12 +20,18 @@ function HomePage() {
                             </div>
                         </form>
                     </div>
+                    <div className="col-2">
+                        <form className="form-inline">
+                            <div className="input-group">
+                                <button className="btn btn-dark" type="submit">Adicionar livro
+                                </button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
 
                 <div className="row gx-5">
-
                     <div className="col">
-
                         <div className="p-3 livros-lista-container">
                             <div className="row p-2">
                                 <h2>Quero Ler</h2>
@@ -38,7 +42,6 @@ function HomePage() {
                                 <ListaLivros livros={livros} />
                             </div>
                         </div>
-
                     </div>
 
                     <div className="col">
@@ -55,23 +58,6 @@ function HomePage() {
                     </div>
                 </div>
             </div>
-
-            {/* <div className="container px-4">
-                <div className="row gx-5">
-
-                    <div className="col livros-lista-container">
-
-
-
-                    </div>
-
-
-                    <div className="col livros-lista-container">
-
-                    </div>
-
-                </div>
-            </div> */}
         </div>
     );
 }
