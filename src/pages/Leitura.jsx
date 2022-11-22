294b19170c97eb13383a08fd4652c57d1949a941
robertomalheiros <robertomalheirosb@gmail.com>
2022-11-21 23:41:27 -0300

    Roberto- Mudei o nome e rota da ultima pagina para Leitura, coloquei um botão em detalhes que leva para leitura, coloquei um pdf local e novo component para Epub.

---
+++ src/pages/Leitura.jsx
@@ -0,0 +1,11 @@
+import BookPdf from "../components/BookPdf";
+
+function Leitura({ livros }) {
+  return (
+    <div className="livro-lista">
+      <BookPdf />
+    </div>
+  );
+}
+
+export default Leitura;
