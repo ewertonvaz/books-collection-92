73f7ad0fa0d33213fe127443a04668119e269c26
robertomalheiros <robertomalheirosb@gmail.com>
2022-11-23 12:32:33 -0300

    Setando a página correta PDF

--- src/pages/Leitura.jsx
+++ src/pages/Leitura.jsx
@@ -1,39 +1,29 @@
 import BookPdf from "../components/BookPdf";
 import BookEpub from "../components/BookEpub";
-import { useState } from "react";
+//import { useState } from "react";
 import { useLocation } from "react-router-dom";
 
 function Leitura() {
-  const [showLeitor, setShowLeitor] = useState(false);
+  //const [showLeitor, setShowLeitor] = useState(false);
   const parametro = useLocation();
   const { livro } = parametro.state;
-  console.log(livro);
   console.log(livro.tipo);
-  console.log(livro.ultPagLida);
+  //console.log(livro.ultPagLida);
 
   return (
     <div className="livro-lista">
-      <div>
-        <button
-          onClick={() => {
-            setShowLeitor(!showLeitor);
-          }}
-        >
-          Epub
-        </button>
-      </div>
       {livro.tipo === "PDF" && (
         <BookPdf
-          tipo={livro.caminho}
+          tipo={livro.tipo}
           ultPagLida={livro.ultPagLida}
-          caminho={livro.tipo}
+          caminho={livro.caminho}
         />
       )}
       {livro.tipo === "Epub" && (
         <BookEpub
-          tipo={livro.caminho}
+          tipo={livro.tipo}
           ultPagLida={livro.ultPagLida}
-          caminho={livro.tipo}
+          caminho={livro.caminho}
         />
       )}
     </div>
