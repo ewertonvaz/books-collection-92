f7565d81e5e5313ade66df39c4ed5c10e7607f87
Ewerton da Costa Vaz <ewerton.vaz@gmail.com>
2022-12-04 19:38:43 -0300

    finalizada a função de convere data UTC do Mongo

--- src/util/date.util.js
+++ src/util/date.util.js
@@ -1,32 +1,26 @@
 function formatDateFromApi(dateStr){
-    console.log(dateStr);
-    const newDate = new Date(dateStr);
-    if (!(newDate instanceof Date)) {
+    const tempDate = new Date(dateStr).toUTCString().split("GMT")[0];
+    const utcDate = new Date(tempDate);
+    //console.log(utcDate);
+    if (!(utcDate instanceof Date)) {
         return null;
     }
-    console.log(newDate);
-    const ano = newDate.getFullYear();
-    const mes =  newDate.getMonth() + 1;
-    const dia = newDate.getDate();
-    console.log(ano, mes, dia);
+    // console.log(utcDate);
+    const ano = utcDate.getFullYear();
+    const mes =  utcDate.getMonth() + 1;
+    const dia = utcDate.getDate();
     const frmDate = ano + "-" + (mes < 10 ? "0" + mes : mes) + "-" + (dia < 10 ? "0" + dia : dia);
-    console.log(frmDate);
     return frmDate;
 }
 
 function formatDateBR(dateStr){
-    const newDate = new Date(dateStr);
-    if (!(newDate instanceof Date)) {
+    const tempDate = new Date(dateStr).toUTCString().split("GMT")[0];
+    const utcDate = new Date(tempDate);
+    if (!(utcDate instanceof Date)) {
         return null;
     }
-    console.log(newDate);
-    // const ano = newDate.getFullYear();
-    // const mes =  newDate.getMonth() + 1;
-    // const dia = newDate.getDate();
-    // console.log(ano, mes, dia);
-    // const frmDate = (dia < 10 ? "0" + dia : dia) + "/" + (mes < 10 ? "0" + mes : mes) + "/" + ano;
-    const frmDate = newDate.toLocaleDateString('pt-BR')
-    console.log(frmDate);
+    //console.log(utcDate);
+    const frmDate = utcDate.toLocaleDateString('pt-BR')
     return frmDate;
 }
 
